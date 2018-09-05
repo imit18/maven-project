49c9a6ab5aad91504a6f5437f573bff646d6d3a1
imit18 <iti.mittal2009@gmail.com>
2018-09-05 06:40:02 +0200

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-  * Returns greeting String
+  * @param someone  name of person
+  * @eeturn greeting String
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
